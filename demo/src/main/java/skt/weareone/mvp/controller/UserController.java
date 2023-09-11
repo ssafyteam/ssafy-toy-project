d3988610eb1997a81d55f910e8c9c14eca606b13
DasolKang <dasol199@naver.com>
2023-09-11 21:30:42 +0900

    :sparkles: Update User Controller for Swagger

--- demo/src/main/java/skt/weareone/mvp/controller/UserController.java
+++ demo/src/main/java/skt/weareone/mvp/controller/UserController.java
@@ -1,21 +1,22 @@
 package skt.weareone.mvp.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import skt.weareone.mvp.dto.request.BlogPostRequest;
+import org.springframework.web.bind.annotation.*;
 import skt.weareone.mvp.dto.request.LoginRequest;
 import skt.weareone.mvp.dto.request.SignupRequest;
+import skt.weareone.mvp.dto.request.UserInfoRequest;
+import skt.weareone.mvp.dto.response.LoginResponse;
+import skt.weareone.mvp.dto.response.UserInfoResponse;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -42,6 +43,8 @@ public class UserController {
 
     /**
      * 로그인
+     * @param loginRequest
+     * @return Login Response
      */
     @PostMapping("/login")
     @Operation(summary = "로그인 메서드", description = "로그인 요청을 받는 메서드 입니다.")
@@ -49,15 +52,78 @@ public class UserController {
             @ApiResponse(responseCode = "200", description = "successful"),
             @ApiResponse(responseCode = "400", description = "bad request operation")
     })
-    public ResponseEntity login(@RequestBody @Valid LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+        LoginResponse response = new LoginResponse(); // 추후 로그인 로직 추가
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+    /**
+     * 로그아웃
+     * @param token
+     * @return
+     */
+    @DeleteMapping("/logout")
+    @Operation(summary = "로그아웃 메서드", description = "로그아웃 요청을 받는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful"),
+            @ApiResponse(responseCode = "400", description = "bad request operation")
+    })
+    public ResponseEntity logout(@RequestHeader("Authorization") String token) {
+
         return new ResponseEntity(HttpStatus.OK);
     }
 
     /**
-     * 인증
+     * 회원 탈퇴
+     * @param token
+     * @return
      */
+    @DeleteMapping("/users")
+    @Operation(summary = "회원탈퇴 메서드", description = "회원탈퇴 요청을 받는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No Content"),
+            @ApiResponse(responseCode = "400", description = "bad request operation")
+    })
+    public ResponseEntity byeUser(@RequestHeader("Authorization") String token) {
+        //회원정보와 연관된 모든 정보 삭제
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    
+    // 인증
+    /**
+     * Refresh Token으로 Access Token 발급
+     * @param refreshToken
+     * @return LoginResponse
+     */
+    @PostMapping("/users/refresh")
+    @Operation(summary = "토큰 재발급 요청", description = "Refresh Token으로 Access Token 새로 발급")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully Created"),
+            @ApiResponse(responseCode = "400", description = "bad request operation")
+    })
+    public ResponseEntity<LoginResponse> requestRefresh(@RequestBody @NotEmpty String refreshToken) {
+        LoginResponse response = new LoginResponse(); // 추후 로직 추가
+        return new ResponseEntity(response, HttpStatus.CREATED);
+    }
+
 
     /**
-     * 내 정보
+     * 내 정보 수정
+     * @param token
+     * @param userInfoRequest
+     * @return userInfoResponse
      */
+    @PostMapping("/users/info")
+    @Operation(summary = "내 정보 수정 요청", description = "정보 수정")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully Created"),
+            @ApiResponse(responseCode = "400", description = "bad request operation")
+    })
+    public ResponseEntity<UserInfoResponse> getMemberInfo(@RequestHeader("Authorization") String token,
+                                        @RequestBody UserInfoRequest userInfoRequest) {
+        UserInfoResponse userInfoResponse = new UserInfoResponse(); // 정보 수정 로직 추가
+        return new ResponseEntity(userInfoResponse, HttpStatus.CREATED);
+    }
+
 }
