474bdf6ff5df95c3175b3d27f7163af11b56913f
hgfdsa4320 <hgfdsa4320@naver.com>
2023-09-12 13:27:37 +0900

    Update FeedController

--- demo/src/main/java/skt/weareone/mvp/controller/FeedController.java
+++ demo/src/main/java/skt/weareone/mvp/controller/FeedController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import skt.weareone.mvp.dto.request.SuggestionRequest;
 import skt.weareone.mvp.dto.response.FeedInfoResponse;
 import skt.weareone.mvp.service.impl.FeedServiceImpl;
 
@@ -17,14 +16,15 @@ public class FeedController {
 
     private final FeedServiceImpl feedService;
 
+    @Operation(summary = "전체 피드 조회 메서드", description = "포스트된 모든 피드들을 확인할 수 있습니다.")
+    @GetMapping
+    public ResponseEntity<FeedInfoResponse> findAllFeed() {
+        return ResponseEntity.ok(feedService.findAllFeed());
+    }
 
-
-    @Operation(summary = "전체 피드 조회 메서드", description = "포스트된 모든 피드들을 확인할 수 있습니다. 검색 조건을 달면 검색도 가능합니다.")
+    @Operation(summary = "검색 조회 메서드", description = "검색 조건을 달면 검색을 가능하게 합니다.")
     @GetMapping
     public ResponseEntity<FeedInfoResponse> findAllFeed(String query) {
-        if (query == null || query.equals("")) {
-            return ResponseEntity.ok(feedService.findAllFeed());
-        }
         return ResponseEntity.ok(feedService.findSearchFeed(query));
     }
 
