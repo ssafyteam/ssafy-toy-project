a3ecc2fcdce2cb9adcaac7b67e81a7009349b1fc
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2023-11-19 14:45:55 +0900

    ⚡ Update: 통합 테스트 및 오류 수정 (#31)
    * ✨ Feat: 로그아웃, 내 정보 기능 추가
    
    * ⚡ Update: 토큰에서 Email 반환 로직 수정
    
    * ⚡ Update: 통합 테스트 및 오류 수정

--- demo/src/main/java/com/weareone/quicklog/dto/request/BlogPostRequest.java
+++ demo/src/main/java/com/weareone/quicklog/dto/request/BlogPostRequest.java
@@ -3,12 +3,14 @@ package com.weareone.quicklog.dto.request;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /*
  * 게시글 작성
  * body: 카테고리, 제목, 작성날짜, 태그들, 내용 (+이미지), 공개/비공개 여부
  */
 @Getter
+@NoArgsConstructor
 public class BlogPostRequest {
     private String category;
     @NotEmpty(message = "제목은 공백일 수 없습니다.")
